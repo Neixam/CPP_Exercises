382e1e6d85c88858c5efd5e3baf052e1656a1f28
neixam <aminebourennane@gmail.com>
2023-01-12 17:16:30 +0100

    Finish part 4

--- TP1/helloworld.cpp
+++ TP1/helloworld.cpp
@@ -1,7 +1,15 @@
+#include <string>
 #include <iostream>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello world !" << std::endl;
+    // int var;
+    string name;
+
+    // cin >> var;
+    cout << "What's your name ?" << endl;
+    cin >> name;
+    cout << "Hello " << name << " !" << endl;
     return 0;
 }
