aa6f1877988d794f1ccbf6048d4f2555d5f2663b
neixam <aminebourennane@gmail.com>
2023-03-22 10:50:33 +0100

    Saved changes

--- src/Kitchen.cpp
+++ src/Kitchen.cpp
@@ -1,1 +1,27 @@
 #include "Kitchen.hpp"
+#include <utility>
+
+const Unit& Kitchen::register_unit(Unit unit)
+{
+    auto [it, emplaced] = _units.emplace(std::move(unit));
+    return *it;
+}
+const Unit* Kitchen::find_unit(const std::string& name) const
+{
+    const auto& p = _units.find(name, [](const auto& n, const auto& u) { return ;});
+    if (p == _units.end())
+        return nullptr;
+    return &p->second;
+}
+const Ingredient& Kitchen::register_ingredient(Ingredient ingredient)
+{
+    _ingredients.try_emplace(ingredient);
+    return ingredient;
+}
+const Ingredient* Kitchen::find_ingredient(const std::string& name) const
+{
+    const auto& p = _ingredients.find(name, [](const auto& i1, const auto& i2) {  });
+    if (p == _ingredients.end())
+        return nullptr;
+    return &p->second;
+}
--- src/Kitchen.hpp
+++ src/Kitchen.hpp
@@ -1,4 +1,21 @@
 #pragma once
 
+#include "../lib/Ingredient.hpp"
+#include "../lib/Unit.hpp"
+
+#include <cstring>
+#include <set>
+#include <string>
+
 class Kitchen
-{};
+{
+public:
+    Kitchen() = default;
+    const Unit& register_unit(Unit unit);
+    const Ingredient& register_ingredient(Ingredient ingredient);
+    const Unit* find_unit(const std::string& name) const;
+    const Ingredient* find_ingredient(const std::string& name) const;
+private:
+    std::set<Unit> _units;
+    std::set<Ingredient> _ingredients;
+};
