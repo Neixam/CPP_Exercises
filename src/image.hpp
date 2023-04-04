fd5a95002232ee5a1152b9647486cd8061a6a1d3
neixam <aminebourennane@gmail.com>
2023-04-04 14:07:41 +0200

    stage change

--- src/image.hpp
+++ src/image.hpp
@@ -11,3 +11,37 @@
 #include <array>
 #include <iostream>
 
+template <typename P, size_t W, size_t H>
+class Image
+{
+public:
+    Image<P, W, H>() = default;
+    ~Image() = default;
+    Image<P, W, H>(const P& pi)
+    {
+        for (auto& line : _img)
+            line.fill(pi);
+    }
+    P& operator()(const size_t i, const size_t j)
+    {
+        return _img[j][i];
+    }
+    const P& operator()(const size_t i, const size_t j) const
+    {
+        return _img[j][i];
+    }
+    Image<P, W, H>(const std::function<P(const size_t i, const size_t j)> & functor)
+    {
+        for (size_t j = 0; j < H; j++)
+            for (size_t i = 0; i < W; i++)
+                _img[j][i] = functor(i, j);
+    }
+private:
+    std::array<std::array<P, W>, H> _img;
+};
+
+template <typename P, size_t W, size_t H>
+Image<P, W, H> operator+(const Image<P, W, H>& i1, const Image<P, W, H>& i2)
+{
+    return Image<P, W, H>([i1, i2](size_t i, size_t j) { return i1(i, j) + i2(i, j); });
+}
--- src/pixels.hpp
+++ src/pixels.hpp
@@ -4,3 +4,89 @@
 
 #include <stdint.h>
 
+struct RGBA
+{
+    uint8_t r;
+    uint8_t g;
+    uint8_t b;
+    uint8_t a;
+};
+
+struct RGB
+{
+    uint8_t r;
+    uint8_t g;
+    uint8_t b;
+};
+
+struct Luma
+{
+    uint8_t gray;
+};
+
+template<typename A, typename B>
+A from(const B & stuff);
+
+template<>
+RGBA from(const RGB& stuff)
+{
+    return {stuff.r, stuff.g, stuff.b, 255};
+}
+
+template<>
+RGB from(const RGB& stuff)
+{
+    return stuff;
+}
+
+template<>
+RGBA from(const RGBA& stuff)
+{
+    return stuff;
+}
+
+template<>
+Luma from(const Luma& stuff)
+{
+    return stuff;
+}
+
+template<>
+RGB from(const RGBA& stuff)
+{
+    return {stuff.r, stuff.g, stuff.b};
+}
+
+template<>
+Luma from(const RGB& stuff)
+{
+    return {lib::rgb_to_grayscale(stuff.r, stuff.g, stuff.b)};
+}
+
+template<>
+RGB from(const Luma& stuff)
+{
+    auto tmp = lib::grayscale_to_rgb(stuff.gray);
+    return reinterpret_cast<RGB&>(tmp);
+}
+
+template<>
+Luma from(const RGBA& stuff)
+{
+    return from<Luma, RGB>(from<RGB, RGBA>(stuff));
+}
+
+template<>
+RGBA from(const Luma& stuff)
+{
+    return from<RGBA, RGB>(from<RGB, Luma>(stuff));
+}
+
+template<typename A, typename B, typename C>
+A operator+(const B& stuff);
+
+RGBA operator+(const RGBA& p1, const RGBA& p2)
+{
+    auto tmp = lib::mix_color(p1.r, p1.g, p1.b, p1.a, p2.r, p2.g, p2.b, p2.a);
+    return {tmp[0], tmp[1], tmp[2], tmp[3]};
+}
